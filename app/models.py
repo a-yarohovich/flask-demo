a09ebcf0da527383f22b9179c5b19a8d5fff95c3
Andrew Yarohovich <yarohovich@gmail.com>
2017-09-17 02:19:38 +0300

    start adding auth for users

--- app/models.py
+++ app/models.py
@@ -1,9 +1,22 @@
 from . import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Role():
     pass
 
 
 class User():
-    pass
+
+    def verify_pass(self, password, username):
+        cursor = db.connect().cursor()
+        cursor.execute("select password from demo.users where username='" + username + "'")
+        data = cursor.fetchone()
+        return check_password_hash(data[0], password=password)
+
+    def set_pass(self, password, username):
+        cursor = db.connect().cursor()
+        password_hash = generate_password_hash(password=password)
+        print(password_hash)
+        query = "insert into demo.users(`password`) values('" + password_hash + "'); commit"
+        print(query)
+        cursor.execute(query=query)
