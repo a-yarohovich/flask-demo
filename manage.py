87852da7644eb867747417f8344f1995e9714fab
Andrew Yarohovich <yarohovich@gmail.com>
2017-09-18 19:56:29 +0300

    add sql dump

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app, db
 from app.models import User, Role
 from flask_script import Manager, Shell
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('DEMO_CONFIG') or 'default')
 manager = Manager(app)
 
 
